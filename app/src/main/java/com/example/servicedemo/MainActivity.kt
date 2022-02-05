bba3e88f0ada9b98e756e25f5a7e1cac2d0fc0ff
Akhilraj Nambiar <nambiarakhilraj01@gmail.com>
2022-02-05 23:10:09 +0530

    Using a BigPicture + MediaStyle to show music player notification

--- app/src/main/java/com/example/servicedemo/MainActivity.kt
+++ app/src/main/java/com/example/servicedemo/MainActivity.kt
@@ -1,21 +1,22 @@
 package com.example.servicedemo
 
-import android.app.NotificationManager
+import android.app.Notification
 import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.media.session.MediaSessionCompat
 import android.widget.Button
 import android.widget.EditText
-import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var notificationManager: NotificationManagerCompat
+    private lateinit var mediaSession: MediaSessionCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         val contentIntent = PendingIntent.getActivity(
             this, 0, activityIntent, 0
         )
+        mediaSession = MediaSessionCompat(this, "tag")
 
         notificationManager = NotificationManagerCompat.from(this)
 
@@ -39,26 +41,42 @@ class MainActivity : AppCompatActivity() {
             this, 0, broadcastIntent, 0
         )
 
+        val picture = BitmapFactory.decodeResource(resources, R.drawable.monkeywithheadphones)
+
         sendToChannel1.setOnClickListener {
             val notification = NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setLargeIcon(picture)
+                .setStyle(NotificationCompat.BigPictureStyle()
+                    .bigPicture(picture)
+                    .bigLargeIcon(null)
+                )
                 .setContentTitle(editTextTitle.text)
                 .setContentText(editTextMessage.text)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .setColor(Color.BLUE)
                 .setContentIntent(contentIntent)
                 .setAutoCancel(true)
                 .setOnlyAlertOnce(true)
-                .addAction(R.mipmap.ic_launcher, "Toast", actionIntent)
                 .build()
 
             notificationManager.notify(1, notification)
         }
 
+        val picture2 = BitmapFactory.decodeResource(resources, R.drawable.meditatingmonkey)
+
         sendToChannel2.setOnClickListener {
             val notification = NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setLargeIcon(picture2)
+                .addAction(R.drawable.ic_prev, "Prev", null)
+                .addAction(R.drawable.ic_play, "Play", null)
+                .addAction(R.drawable.ic_next, "Next", null)
+                .setStyle(
+                    androidx.media.app.NotificationCompat.MediaStyle()
+                        .setMediaSession(mediaSession.sessionToken)
+                )
+                .setSubText("Sub text")
                 .setContentTitle(editTextTitle.text)
                 .setContentText(editTextMessage.text)
                 .setPriority(NotificationCompat.PRIORITY_LOW)
